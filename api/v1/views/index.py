8993a992e091d32443fe821f6a0d69d2c94d2ecd
7asanka <hassan2002_kimo@hotmail.com>
2025-02-15 09:52:31 -0800

    typo fix

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -5,7 +5,7 @@ from api.v1.views import app_views
 from flask import jsonify
 
 
-@app_viesw.route('/status', strict_slashes=False)
+@app_view.route('/status', strict_slashes=False)
 def status():
     """returns status"""
     return jsonify({"status": "OK"})
